311877938c456107a4b53a216ae43438143b232e
James <sep7696@gmail.com>
2025-05-16 18:07:57 +0330

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { invoke } from "@tauri-apps/api/core";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+ async translate(word: string) {
+    const result = await invoke('translate', { word });
+    alert(result);
+  }
+}
